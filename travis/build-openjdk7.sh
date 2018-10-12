cd06418d6018445184d9258fae442fe6b6647ee8
Mickaël BORNE <mickael.borne@ign.fr>
2018-10-12 16:59:27 +0200

    travis - fix build using jdk_switcher instead of switching between trusty or xenial (closes #43)

---
+++ travis/build-openjdk7.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://github.com/travis-ci/travis-ci/issues/8681#issuecomment-340821970
+
+source /opt/jdk_switcher/jdk_switcher.sh
+
+# https://github.com/INRIA/spoon/blob/master/chore/travis/travis-jdk8.sh
+jdk_switcher use openjdk7 && mvn -Djava.src.version=1.7 clean package
+
