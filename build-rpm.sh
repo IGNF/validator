3808eee527bc443d58ec8ec2f4e22173d22c5b32
Mickaël BORNE <mickael.borne@ign.fr>
2019-07-22 15:30:52 +0200

    packaging - ensure fpm exists and display link to installation doc (refs #118)

--- build-rpm.sh
+++ build-rpm.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+hash fpm 2>/dev/null || { echo >&2 "ERROR : fpm is required to build rpm package (see https://fpm.readthedocs.io/en/latest/installing.html)"; exit 1; }
+
 cd validator-cli/target
 
 VERSION=$(java -jar validator-cli.jar version)
