dc699504a9984e9f7b591013ae607be317b9f90b
MBorne <mborne@users.noreply.github.com>
2019-07-23 23:23:50 +0200

    travis (refs #80)

--- travis/build-openjdk11.sh
+++ travis/build-openjdk11.sh
@@ -7,5 +7,5 @@ wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
 chmod +x install-jdk.sh
 
 source ./install-jdk.sh -f 11 -c
-mvn -Djava.src.version=1.8 clean package
+mvn clean package
 
