fbe1b649b660d74a7c8be6600e95bd9fe5cf0907
MBorne <mborne@users.noreply.github.com>
2019-07-23 23:38:04 +0200

    jts upgrade - ensure that 3D geometry are now supported (refs #80)

--- validator-core/src/test/java/fr/ign/validator/model/type/PointTypeTest.java
+++ validator-core/src/test/java/fr/ign/validator/model/type/PointTypeTest.java
@@ -7,71 +7,67 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 
-
 public class PointTypeTest extends AbstractTypeTest<Geometry> {
 
 	public PointTypeTest() {
 		super(new PointType());
 	}
-	
+
 	@Test
-	public void testBindEmpty(){
-		Geometry geometry = type.bind("GEOMETRYCOLLECTION EMPTY") ;
-		assertTrue( geometry.isEmpty() );
+	public void testBindEmpty() {
+		Geometry geometry = type.bind("GEOMETRYCOLLECTION EMPTY");
+		assertTrue(geometry.isEmpty());
 	}
-	
+
 	@Test
-	public void testBindPoint(){
-		Geometry geometry = type.bind("POINT(3.0 4.0)") ;
-		assertFalse( geometry.isEmpty() );
-		assertTrue(geometry instanceof Point);
-	}
-	
-	/**
-	 * JTS does not support Z notation before 1.15
-	 */
-	@Test
-	@Ignore
-	public void testBindPointZ(){
-		Geometry geometry = type.bind("POINT Z (809848.050930752 6322607.71635569 0)");
-		assertFalse( geometry.isEmpty() );
+	public void testBindPoint() {
+		Geometry geometry = type.bind("POINT(3.0 4.0)");
+		assertFalse(geometry.isEmpty());
 		assertTrue(geometry instanceof Point);
 	}
 
 	@Test
-	public void testBindFakeMultiPoint(){
-		Geometry geometry = type.bind("MULTIPOINT((3.0 4.0))") ;
-		assertFalse( geometry.isEmpty() );
+	public void testBindPointZ() {
+		Geometry geometry = type.bind("POINT Z (809848 6322607 8)");
+		assertFalse(geometry.isEmpty());
+		assertTrue(geometry instanceof Point);
+		Point p = (Point) geometry;
+		assertEquals("(809848.0, 6322607.0, 8.0)", p.getCoordinate().toString());		
+	}
+
+	@Test
+	public void testBindFakeMultiPoint() {
+		Geometry geometry = type.bind("MULTIPOINT((3.0 4.0))");
+		assertFalse(geometry.isEmpty());
 		assertTrue(geometry instanceof Point);
 	}
-	
+
 	@Test
-	public void testBindFakeGeometryCollection(){
-		Geometry geometry = type.bind("GEOMETRYCOLLECTION(POINT(3.0 4.0))") ;
-		assertFalse( geometry.isEmpty() );
+	public void testBindFakeGeometryCollection() {
+		Geometry geometry = type.bind("GEOMETRYCOLLECTION(POINT(3.0 4.0))");
+		assertFalse(geometry.isEmpty());
 		assertTrue(geometry instanceof Point);
 	}
-	
+
 	@Test
-	public void testBindLineString(){
-		boolean throwException = false ;
+	public void testBindLineString() {
+		boolean throwException = false;
 		try {
-			type.bind("LINESTRING(2.0 3.0,4.0 5.0)") ;
-		}catch (IllegalArgumentException e){
-			throwException = true ;
+			type.bind("LINESTRING(2.0 3.0,4.0 5.0)");
+		} catch (IllegalArgumentException e) {
+			throwException = true;
 		}
 		assertTrue(throwException);
 	}
-	
+
 	@Test
-	public void testBindRealGeometryCollection(){
-		boolean throwException = false ;
+	public void testBindRealGeometryCollection() {
+		boolean throwException = false;
 		try {
-			type.bind("GEOMETRYCOLLECTION(POINT(3.0 4.0),POINT(3.0 4.0))") ;
-		}catch (IllegalArgumentException e){
-			throwException = true ;
+			type.bind("GEOMETRYCOLLECTION(POINT(3.0 4.0),POINT(3.0 4.0))");
+		} catch (IllegalArgumentException e) {
+			throwException = true;
 		}
 		assertTrue(throwException);
 	}
 }
-
