7d4cd10fe69789c60516079423969db5aaa90ada
Mickaël BORNE <mickael.borne@ign.fr>
2019-10-14 16:55:19 +0200

    simplify travis/build-openjdk11.sh (refs #110)

--- travis/build-openjdk11.sh
+++ travis/build-openjdk11.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-# see https://github.com/INRIA/spoon/blob/master/chore/travis/
-
-# source /opt/jdk_switcher/jdk_switcher.sh
-# wget https://raw.githubusercontent.com/sormuras/bach/master/install-jdk.sh
-# chmod +x install-jdk.sh
-
-# source ./install-jdk.sh -f 11 -c
 mvn clean package
 
