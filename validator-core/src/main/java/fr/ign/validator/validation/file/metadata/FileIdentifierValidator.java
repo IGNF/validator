18454ba662e2638bfa5607dd2397d4b8f601bd39
MBorne <mickael.borne@ign.fr>
2020-06-23 17:47:16 +0200

    validator-core - METADATA_FILEIDENTIFIER_INVALID - allow more characters in fileIdentifier regex (closes #159)

--- validator-core/src/main/java/fr/ign/validator/validation/file/metadata/FileIdentifierValidator.java
+++ validator-core/src/main/java/fr/ign/validator/validation/file/metadata/FileIdentifierValidator.java
@@ -23,7 +23,18 @@ public class FileIdentifierValidator implements Validator<Metadata> {
 	public static final Logger log    = LogManager.getRootLogger() ;
 	public static final Marker MARKER = MarkerManager.getMarker("FileIdentifierValidator") ;	
 	
-	public static final String REGEXP = "[\\w-.:]+";
+	/**
+	 * Allows [a-z][A-Z][0-9], spaces, "-", ".", ":" "/" "\"
+	 */
+	public static final String REGEXP = "[\\w\\s-.:/\\[\\]]+";
+	
+
+	public boolean isValid(String fileIdentifier) {
+		if ( StringUtils.isEmpty(fileIdentifier) ) {
+			return false;
+		}
+		return fileIdentifier.matches(REGEXP);
+	}
 	
 	@Override
 	public void validate(Context context, Metadata metadata) {
@@ -35,12 +46,13 @@ public class FileIdentifierValidator implements Validator<Metadata> {
 			);
 			return;
 		}
-		if ( ! fileIdentifier.matches(REGEXP) ){
+		if ( ! isValid(fileIdentifier) ){
 			context.report(context.createError(CoreErrorCodes.METADATA_FILEIDENTIFIER_INVALID)
 				.setMessageParam("VALUE", fileIdentifier)
 				.setMessageParam("EXPECTED_REGEXP", REGEXP)
 			);
 		}
 	}
+
 
 }
--- validator-core/src/test/java/fr/ign/validator/validation/file/metadata/FileIdentifierValidatorTest.java
+++ validator-core/src/test/java/fr/ign/validator/validation/file/metadata/FileIdentifierValidatorTest.java
@@ -1,9 +1,16 @@
 package fr.ign.validator.validation.file.metadata;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -11,6 +18,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import fr.ign.validator.error.CoreErrorCodes;
 import fr.ign.validator.error.ValidatorError;
 import fr.ign.validator.metadata.Metadata;
+import fr.ign.validator.tools.ResourceHelper;
 
 @RunWith(MockitoJUnitRunner.class)
 public class FileIdentifierValidatorTest extends MetadataValidatorTestBase {
@@ -25,7 +33,7 @@ public class FileIdentifierValidatorTest extends MetadataValidatorTestBase {
 
 		assertEquals(0, report.getErrors().size());
 	}
-	
+
 	@Test
 	public void testValid2() {
 		Metadata metadata = mock(Metadata.class);
@@ -65,23 +73,38 @@ public class FileIdentifierValidatorTest extends MetadataValidatorTestBase {
 		assertEquals("Le champ \"fileIdentifier\" n'est pas renseigné.", error.getMessage());
 	}
 
+
 	@Test
-	public void testInvalidSpaces() {
+	public void testValidClean() {
+		Metadata metadata = mock(Metadata.class);
+		when(metadata.getFileIdentifier()).thenReturn("fr-000080230-CC20110701");
+
+		FileIdentifierValidator validator = new FileIdentifierValidator();
+		validator.validate(context, metadata);
+
+		assertEquals(0, report.getErrors().size());
+	}
+	
+	public void testValidWithExtension() {
+		Metadata metadata = mock(Metadata.class);
+		when(metadata.getFileIdentifier()).thenReturn("fr-200034684-88104plu20071210.xml");
+
+		FileIdentifierValidator validator = new FileIdentifierValidator();
+		validator.validate(context, metadata);
+
+		assertEquals(0, report.getErrors().size());
+	}
+
+	@Test
+	public void testValidWithSpaces() {
 		Metadata metadata = mock(Metadata.class);
 		when(metadata.getFileIdentifier()).thenReturn("my fileidentifier");
 
 		FileIdentifierValidator validator = new FileIdentifierValidator();
 		validator.validate(context, metadata);
 
-		assertEquals(1, report.getErrors().size());
-		ValidatorError error = report.getErrors().get(0);
-		assertEquals(CoreErrorCodes.METADATA_FILEIDENTIFIER_INVALID, error.getCode());
-		assertEquals(
-			"Le champ \"fileIdentifier\" (my fileidentifier) ne correspond pas à l'expression régulière ([\\w-.:]+).",
-			error.getMessage()
-		);
+		assertEquals(0, report.getErrors().size());
 	}
-
 
 	@Test
 	public void testInvalidSpecialChars() {
@@ -95,9 +118,28 @@ public class FileIdentifierValidatorTest extends MetadataValidatorTestBase {
 		ValidatorError error = report.getErrors().get(0);
 		assertEquals(CoreErrorCodes.METADATA_FILEIDENTIFIER_INVALID, error.getCode());
 		assertEquals(
-			"Le champ \"fileIdentifier\" (special!) ne correspond pas à l'expression régulière ([\\w-.:]+).",
+			"Le champ \"fileIdentifier\" (special!) ne correspond pas aux caractères autorisés.",
 			error.getMessage()
 		);
 	}
-	
+
+
+
+	/**
+	 * Ensure that all fileIdentifiers in are valid in "sample-fileidentifiers-valid.txt" file
+	 * @throws IOException 
+	 */
+	@Test
+	public void testRegressSampleFileidentifiersValid() throws IOException {
+		File file = ResourceHelper.getResourceFile(getClass(), "/metadata/sample-fileidentifiers-valid.txt");
+		List<String> fileIdentifiers = FileUtils.readLines(file,StandardCharsets.UTF_8);
+		FileIdentifierValidator validator = new FileIdentifierValidator();
+		for (String fileIdentifier : fileIdentifiers) {
+			assertTrue(
+				String.format("'%s' should be valid",fileIdentifier),
+				validator.isValid(fileIdentifier)
+			);
+		}
+	}
+
 }
