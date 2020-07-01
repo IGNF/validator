995b77da7087eacd96413b2546ba31b8519864b9
MBorne <mickael.borne@ign.fr>
2020-07-01 13:03:50 +0200

    validator-plugin-cnig - VRT - use relative path for CSV (closes #169)

---
+++ validator-plugin-cnig/src/test/java/fr/ign/validator/cnig/tools/VRTTest.java
@@ -0,0 +1,53 @@
+package fr.ign.validator.cnig.tools;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import fr.ign.validator.cnig.CnigRegressHelper;
+import fr.ign.validator.model.DocumentModel;
+import fr.ign.validator.model.FeatureType;
+import fr.ign.validator.tools.ResourceHelper;
+
+/**
+ * Test VRT generation
+ * 
+ * TODO add test with a geom
+ * 
+ * @author MBorne
+ *
+ */
+public class VRTTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testDocUrba() throws Exception {
+        // Copy DOC_URBA.csv
+        File csvFile = folder.newFile("DOC_URBA.csv");
+        FileUtils.copyFile(
+            ResourceHelper.getResourceFile(getClass(), "/csv/DOC_URBA.csv"),
+            csvFile
+        );
+
+        // Retrieve FeatureType
+        DocumentModel documentModel = CnigRegressHelper.getDocumentModel("cnig_PLU_2017");
+        FeatureType featureType = documentModel.getFileModelByName("DOC_URBA").getFeatureType();
+        File vrtFile = VRT.createVRTfile(csvFile, featureType);
+        assertTrue(vrtFile.exists());
+
+        File expectedVrtFile = ResourceHelper.getResourceFile(getClass(), "/csv/DOC_URBA.vrt");
+        String expected = FileUtils.readFileToString(expectedVrtFile, StandardCharsets.UTF_8);
+        String actual = FileUtils.readFileToString(vrtFile, StandardCharsets.UTF_8);
+        assertEquals(expected, actual);
+    }
+
+}
