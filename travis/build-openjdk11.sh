52c8fc70393c42bc7b7f7712f0001e052631771e
MBorne <mborne@users.noreply.github.com>
2021-07-11 15:11:01 +0200

    travis - set locale to fr_FR.UTF-8 to test XSD messages... (refs #231)

--- travis/build-openjdk11.sh
+++ travis/build-openjdk11.sh
@@ -4,6 +4,10 @@ echo "-----------------------------------------------------------------------"
 echo "-- Display informations"
 echo "-----------------------------------------------------------------------"
 
+# XSD validation errors
+export LANG=fr_FR.UTF-8
+export LC_ALL=fr_FR.UTF-8
+
 export OGR2OGR_PATH=${OGR2OGR_PATH:-ogr2ogr}
 echo "-- OGR2OGR_PATH : ${OGR2OGR_PATH}"
 echo "-- GDAL_VERSION : $($OGR2OGR_PATH --version)"
