8e061743de59ac630d8a780706af21ac6e5f332a
cboucheIGN <clement.bouche@ign.fr>
2022-11-02 15:48:54 +0100

    mvn format

--- validator-plugin-pcrs/src/test/java/fr/ign/validator/pcrs/PcrsContextTest.java
+++ validator-plugin-pcrs/src/test/java/fr/ign/validator/pcrs/PcrsContextTest.java
@@ -14,18 +14,17 @@ public class PcrsContextTest {
     @Test
     public void testDefaultConstructor() {
         Context context = new Context();
-       
+
         Assert.assertEquals("UTF-8", context.getEncoding().name());
         Assert.assertFalse(context.isFlatValidation());
     }
-    
-    
+
     @Test
     public void testPcrsDefaultConstructor() {
         Context context = new Context();
         PluginManager pluginManager = new PluginManager();
         pluginManager.getPluginByName("PCRS").setup(context);
-       
+
         Assert.assertEquals("UTF-8", context.getEncoding().name());
         Assert.assertTrue(context.isFlatValidation());
     }
